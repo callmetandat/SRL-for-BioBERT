23b90d5bfecf7a08a4164f0982f67b8bae48e4c2
callmetandat <letandat15082002@gmail.com>
2023-07-06 02:57:11 +0700

    SRL-03|Modify data_preparation.py

--- fine_tune.py
+++ fine_tune.py
@@ -13,7 +13,7 @@ def main():
     tokenizer = BertTokenizer.from_pretrained(BERT_MODEL, do_lower_case=True,truncation=True)
     print(tokenizer)
     # BertTokenizer(name_or_path='dmis-lab/biobert-base-cased-v1.2', vocab_size=28996, model_max_length=1000000000000000019884624838656, is_fast=False, padding_side='right', truncation_side='right', special_tokens={'unk_token': '[UNK]', 'sep_token': '[SEP]', 'pad_token': '[PAD]', 'cls_token': '[CLS]', 'mask_token': '[MASK]'}, clean_up_tokenization_spaces=True)
-
+    
 
 if __name__ == "__main__":
     main()
